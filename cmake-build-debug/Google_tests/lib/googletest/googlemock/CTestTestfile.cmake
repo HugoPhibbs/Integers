fbe26cbda81001ede7198cac23b1f8cb11120fc2
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-15 17:54:27 +1200

    Google test files

---
+++ cmake-build-debug/Google_tests/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Hugo/Documents/Uni/Cosc/Integers/Google_tests/lib/googletest/googlemock
+# Build directory: C:/Users/Hugo/Documents/Uni/Cosc/Integers/cmake-build-debug/Google_tests/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ cmake-build-debug/Google_tests/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Hugo/Documents/Uni/Cosc/Integers/Google_tests/lib/googletest/googletest
+# Build directory: C:/Users/Hugo/Documents/Uni/Cosc/Integers/cmake-build-debug/Google_tests/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
